5d3d50774f579dd624fc740fd6a716d164d06426
katsuwo <ubuntu@ip-172-31-2-25.ap-northeast-1.compute.internal>
2021-07-13 22:10:43 +0000

    eachmethod

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,20 +1,16 @@
-num_max=100
-(1..num_max).each do|num|
-  def fizzbuzz(num)
-    if num % 3 ==0 && num % 5 ==0
-      "FizzBuzz"
-    elsif num % 3 ==0
-      "Fizz"
-    elsif num % 5 ==0
-      "Buzz"
-    else
-      num
-    end
+def fizzbuzz(num)
+  if num % 3 ==0 && num % 5 ==0
+    "FizzBuzz"
+  elsif num % 3 ==0
+    "Fizz"
+  elsif num % 5 ==0
+    "Buzz"
+  else
+    num
   end
-  puts fizzbuzz(num)
 end
 
-puts fizzbuzz(1)
-puts fizzbuzz(3)
-puts fizzbuzz(5)
-puts fizzbuzz(15)
+num_max=100
+(1..num_max).each do|num|
+  puts fizzbuzz(num)
+end
